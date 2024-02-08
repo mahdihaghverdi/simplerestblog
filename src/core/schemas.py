2b3e0cb7883b00200622f515720a0daa5f756fe5
mahdihaghverdi <mahdihaghverdiliewpl@gmail.com>
2024-02-08 14:53:18 +0330

    define user needed schemas

---
+++ src/core/schemas.py
@@ -0,0 +1,33 @@
+from typing import Annotated
+
+from pydantic import BaseModel, EmailStr, constr, AfterValidator
+
+
+def remove_at_sign(str_id: str) -> str:
+    # add @ if it is not present
+    if not str_id.startswith("@"):
+        return str_id
+    return str_id[1:]
+
+
+NoAtStr = Annotated[str, AfterValidator(remove_at_sign)]
+
+
+class _UserSchema(BaseModel):
+    username: str
+    name: str | None = None
+    bio: str | None = None
+    email: EmailStr | None = None
+    telegram: NoAtStr | None = None
+    instagram: NoAtStr | None = None
+    twitter: NoAtStr | None = None
+
+
+class UserSignupSchema(_UserSchema):
+    password: constr(strip_whitespace=True, min_length=8)
+
+
+class UserOutSchema(_UserSchema):
+    telegram: str | None = None
+    instagram: str | None = None
+    twitter: str | None = None
