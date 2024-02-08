6e1c4e26ba99a863e3a885194feb99ead2f971d4
mahdihaghverdi <mahdihaghverdiliewpl@gmail.com>
2024-02-08 12:46:29 +0330

    include user routes in app

--- src/web/app.py
+++ src/web/app.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI
 
+from src.core.config import settings
+from src.web.user_route import router as user_router
+
 app = FastAPI(debug=True)
 
-
-@app.get("/")
-async def hello():
-    return "Hello World"
+PREFIX = f"/api/{settings.API_VERSION}"
+app.include_router(user_router, tags=["users"], prefix=PREFIX)
