1e38a88162f56d3debe046c5b7bd701a4bd08377
mahdihaghverdi <mahdihaghverdiliewpl@gmail.com>
2024-02-08 15:05:17 +0330

    set up the signup route

--- src/web/user_route.py
+++ src/web/user_route.py
@@ -1,8 +1,22 @@
-from fastapi import APIRouter
+from typing import Annotated
+
+from fastapi import APIRouter, Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+from starlette import status
+
+from src.core.database import get_db
+from src.core.schemas import UserOutSchema, UserSignupSchema
 
 router = APIRouter(prefix="/users")
 
 
-@router.get("")
-async def hi():
-    return "Hello there"
+@router.post(
+    "/signup",
+    response_model=UserOutSchema,
+    status_code=status.HTTP_201_CREATED,
+)
+async def signup(
+    user_data: UserSignupSchema,
+    db: Annotated[AsyncSession, Depends(get_db)],
+):
+    return user_data
