f98c23da99db6300bc762dd21bba5fab3d0f0ffd
mahdihaghverdi <mahdihaghverdiliewpl@gmail.com>
2024-02-09 21:16:21 +0330

    make saving the optional data before calling the repo

--- src/service/user_service.py
+++ src/service/user_service.py
@@ -1,5 +1,3 @@
-from zoneinfo import ZoneInfo
-
 from src.core.exceptions import DuplicateUsernameError
 from src.core.schemas import UserSignupSchema, UserSchema
 from src.core.security import hash_password
@@ -10,14 +8,14 @@ from src.service import Service
 class UserService(Service[UserRepo]):
     async def signup_user(self, user_data: UserSignupSchema) -> UserSchema:
         user_data.password = hash_password(user_data.password)
+        if user_data.telegram is not None:
+            user_data.telegram = f"https://t.me/{user_data.telegram}"
+        if user_data.instagram is not None:
+            user_data.instagram = f"https://instagram.com/{user_data.instagram}"
+        if user_data.twitter is not None:
+            user_data.twitter = f"https://x.com/@{user_data.twitter}"
+
         user = await self.repo.add(user_data)
         if user is None:
             raise DuplicateUsernameError(user_data.username)
-        user.created = user.created.astimezone(ZoneInfo("Asia/Tehran"))
-        if user.telegram is not None:
-            user.telegram = f"https://t.me/{user.telegram}"
-        if user.instagram is not None:
-            user.instagram = f"https://instagram.com/{user.instagram}"
-        if user.twitter is not None:
-            user.twitter = f"https://x.com/@{user.twitter}"
         return user
