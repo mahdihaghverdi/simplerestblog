a2329fb1f883964494b90934bf54859bea86a0d9
mahdihaghverdi <mahdihaghverdiliewpl@gmail.com>
2024-02-09 17:00:37 +0330

    test signup

---
+++ tests/test_users.py
@@ -0,0 +1,54 @@
+from src.core.schemas import UserOutSchema
+from src.web.app import PREFIX
+
+
+def test_signup(client):
+    response = client.post(
+        f"{PREFIX}/users/signup",
+        json={"username": "Mahdi", "password": "12345678"},
+    )
+    assert response.status_code == 201, response.text
+
+    data = UserOutSchema(**response.json())
+    assert data.username == "mahdi"
+    for k, v in data.model_dump().items():
+        if k == "username":
+            continue
+        assert v is None
+
+
+def test_signup_with_data(client):
+    data = {
+        "username": "Mahdi",
+        "password": "12345678",
+        "name": "Mahdi Haghverdi",
+        "bio": "Hi I am a writer",
+        "email": "mahdi@mahdi.com",
+        "telegram": "pyeafp",
+        "instagram": "mah.dihaghverdi",
+        "twitter": "mliewpl",
+    }
+
+    response = client.post(f"{PREFIX}/users/signup", json=data)
+    assert response.status_code == 201, response.text
+
+    got = UserOutSchema(**response.json())
+    assert got.username == data["username"].lower()
+    assert got.name == data["name"]
+    assert got.bio == data["bio"]
+    assert got.email == data["email"]
+    assert got.telegram == f'https://t.me/{data["telegram"]}'
+    assert got.instagram == f'https://instagram.com/{data["instagram"]}'
+    assert got.twitter == f'https://x.com/@{data["twitter"]}'
+
+
+def test_signup_duplicate(client):
+    client.post(
+        f"{PREFIX}/users/signup",
+        json={"username": "Mahdi", "password": "12345678"},
+    )
+    response = client.post(
+        f"{PREFIX}/users/signup",
+        json={"username": "Mahdi", "password": "12345678"},
+    )
+    assert response.status_code == 400, response.text
