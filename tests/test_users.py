1b57a44f1fb584e11e2d4632b93aa9a93e9cb67b
mahdihaghverdi <mahdihaghverdiliewpl@gmail.com>
2024-02-11 20:46:05 +0330

    add more tests for `/users/me` and JWT token

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,4 +1,5 @@
 from src.core.config import settings
+from src.core.enums import UserRolesEnum
 from src.core.schemas import UserOutSchema, TokenData
 from src.core.security import create_access_token
 
@@ -89,10 +90,25 @@ class TestAuth:
         assert access_token
         assert token_type.lower() == "bearer"
 
+    def test_access_token_wrong_credentials(self, client):
+        client.post(
+            f"{settings.PREFIX}/auth/access-token",
+            data=self.access_token_data,
+        )
         response = client.post(
             f"{settings.PREFIX}/auth/access-token",
             data={
-                "username": "hello",
+                "username": "string",
+                "grant_type": "password",
+                "password": "12345677",
+            },
+        )
+        assert response.status_code == 401, response.text
+
+        response = client.post(
+            f"{settings.PREFIX}/auth/access-token",
+            data={
+                "username": "str",
                 "grant_type": "password",
                 "password": "12345678",
             },
@@ -137,3 +153,51 @@ class TestAuth:
             headers={"Authorization": f"Bearer {access_token}"},
         )
         assert response.status_code == 401, response.text
+
+
+def test_users_me(client, create_admin, admin_access_token):
+    response = client.get(
+        f"{settings.PREFIX}/users/me",
+        headers={"Authorization": f"Bearer {admin_access_token}"},
+    )
+    assert response.status_code == 200, response.text
+
+    data = response.json()
+    assert data["username"] == "admin"
+    for k, v in data.items():
+        if k == "username":
+            continue
+        assert v is None
+
+
+def test_users_me_with_data(client):
+    data = {
+        "username": "Mahdi",
+        "password": "12345678",
+        "name": "Mahdi Haghverdi",
+        "bio": "Hi I am a writer",
+        "email": "mahdi@mahdi.com",
+        "telegram": "@pyeafp",
+        "instagram": "mah.dihaghverdi",
+        "twitter": "@mliewpl",
+    }
+
+    client.post(f"{settings.PREFIX}/users/signup", json=data)
+    access_token = create_access_token(
+        TokenData(username="mahdi", role=UserRolesEnum.USER),
+    )
+
+    response = client.get(
+        f"{settings.PREFIX}/users/me",
+        headers={"Authorization": f"Bearer {access_token}"},
+    )
+    assert response.status_code == 200, response.text
+
+    got = UserOutSchema(**response.json())
+    assert got.username == data["username"].lower()
+    assert got.name == data["name"]
+    assert got.bio == data["bio"]
+    assert got.email == data["email"]
+    assert got.telegram == f'https://t.me/{data["telegram"][1:]}'
+    assert got.instagram == f'https://instagram.com/{data["instagram"]}'
+    assert got.twitter == f'https://x.com/@{data["twitter"][1:]}'
