e14193c0c685a2bfb95ff2a75e9a9db73e537f0b
mahdihaghverdi <mahdihaghverdiliewpl@gmail.com>
2024-02-14 21:53:32 +0330

    test get all drafts

--- tests/test_drafts.py
+++ tests/test_drafts.py
@@ -1,4 +1,5 @@
 from src.core.config import settings
+from src.core.enums import APIPrefixesEnum
 
 
 def test_create_draft(client, create_admin, admin_auth_headers):
@@ -17,7 +18,7 @@ def test_create_draft(client, create_admin, admin_auth_headers):
 
 def test_get_one_draft(client, create_mahdi, mahdi_auth_headers):
     draft_id = client.post(
-        f"{settings.PREFIX}/drafts",
+        f"{settings.PREFIX}/drafts/create",
         json={"title": "title", "body": "body"},
         headers=mahdi_auth_headers,
     ).json()["id"]
@@ -32,3 +33,28 @@ def test_get_one_draft(client, create_mahdi, mahdi_auth_headers):
     assert data["title"] == "title"
     assert data["body"] == "body"
     assert data["username"] == "mahdi"
+
+
+def test_get_all_drafts(client, create_mahdi, mahdi_auth_headers):
+    def _():
+        return client.post(
+            f"{settings.PREFIX}/drafts/create",
+            json={"title": "title", "body": "body"},
+            headers=mahdi_auth_headers,
+        ).json()["id"]
+
+    ids = [_(), _()]
+    response = client.get(
+        f"{settings.PREFIX}/drafts/all",
+        headers=mahdi_auth_headers,
+    )
+    assert response.status_code == 200, response.text
+
+    data = response.json()
+    for draft, id_ in zip(data, reversed(ids)):
+        assert draft["title"] == "title"
+        assert draft["updated"] is None
+        assert draft["link"] == [
+            "GET",
+            f"{settings.PREFIX}/{APIPrefixesEnum.DRAFTS.value}/{id_}",
+        ]
