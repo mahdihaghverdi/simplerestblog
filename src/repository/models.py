b156c5f6989618e23d5b67342fba729d2d984f9e
mahdihaghverdi <mahdihaghverdiliewpl@gmail.com>
2024-02-16 15:16:35 +0330

    design, make migrations and migrate database for posts with tags and comments;

--- src/repository/models.py
+++ src/repository/models.py
@@ -1,9 +1,10 @@
 import zoneinfo
 from datetime import datetime
 
-from sqlalchemy import Integer, BigInteger, String, DateTime, ForeignKey
+from sqlalchemy import Integer, BigInteger, String, DateTime, ForeignKey, Table, Column
 from sqlalchemy.ext.asyncio import AsyncAttrs
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
+from sqlalchemy_utils import LtreeType
 
 
 def utcnow():
@@ -40,6 +41,14 @@ class UserModel(Base):
         back_populates="user",
         cascade="delete, delete-orphan",
     )
+    posts: Mapped[list["PostModel"]] = relationship(
+        back_populates="user",
+        cascade="delete, delete-orphan",
+    )
+    comments: Mapped[list["CommentModel"]] = relationship(
+        back_populates="user",
+        cascade="delete, delete-orphan",
+    )
 
     def __repr__(self):
         return f"<User: {self.username!r}>"
@@ -53,11 +62,85 @@ class DraftModel(Base):
     updated: Mapped[datetime | None] = mapped_column(DateTime(timezone=True))
     draft_hash: Mapped[str]
 
-    # relation
+    # FK
     username: Mapped[str] = mapped_column(
         ForeignKey(f"{UserModel.__tablename__}.username"),
     )
+
+    # relations
     user: Mapped["UserModel"] = relationship(back_populates="drafts")
+    post: Mapped["PostModel"] = relationship(
+        back_populates="draft",
+        cascade="delete, delete-orphan",
+    )
 
     def __repr__(self):
         return f"<Draft: {self.title!r}>"
+
+
+class PostModel(Base):
+    __tablename__ = "posts"
+
+    slug: Mapped[str]
+    published: Mapped[datetime] = mapped_column(DateTime(timezone=True))
+
+    # FK
+    draft_id: Mapped[int] = mapped_column(
+        ForeignKey(f"{DraftModel.__tablename__}.id"),
+    )
+    username: Mapped[str] = mapped_column(
+        ForeignKey(f"{UserModel.__tablename__}.username"),
+    )
+
+    # relations
+    draft: Mapped["DraftModel"] = relationship(back_populates="post")
+    user: Mapped["UserModel"] = relationship(back_populates="posts")
+    tags: Mapped[set["TagModel"]] = relationship(secondary="association_table")
+    comments: Mapped[list["CommentModel"]] = relationship(
+        back_populates="post",
+        cascade="delete, delete-orphan",
+    )
+
+    def __repr__(self):
+        return f"<Post:{self.slug!r}>"
+
+
+class TagModel(Base):
+    __tablename__ = "tags"
+
+    tag: Mapped[str] = mapped_column(unique=True)
+
+    def __repr__(self):
+        return f"<TagModel: name={self.tag!r}>"
+
+
+association_table = Table(
+    "association_table",
+    Base.metadata,
+    Column("post_id", ForeignKey(f"{PostModel.__tablename__}.id"), primary_key=True),
+    Column("tag_id", ForeignKey(f"{TagModel.__tablename__}.id"), primary_key=True),
+)
+
+
+class CommentModel(Base):
+    __tablename__ = "comments"
+
+    comment: Mapped[str] = mapped_column(String(256))
+    path: Mapped[str | None] = mapped_column(LtreeType)
+    commented: Mapped[datetime] = mapped_column(DateTime(timezone=True))
+    updated: Mapped[datetime | None] = mapped_column(DateTime(timezone=True))
+
+    # FK
+    post_id: Mapped[int] = mapped_column(
+        ForeignKey(f"{PostModel.__tablename__}.id"),
+    )
+    parent_id: Mapped[int | None] = mapped_column(ForeignKey("comments.id"))
+    username: Mapped[str] = mapped_column(
+        ForeignKey(f"{UserModel.__tablename__}.username"),
+    )
+
+    # relations
+    post: Mapped["PostModel"] = relationship(back_populates="comments")
+    children: Mapped[list["CommentModel"]] = relationship(
+        cascade="delete, delete-orphan",
+    )
