f1b910b005559644a29df9699cec54534d3d56d9
mahdihaghverdi <mahdihaghverdiliewpl@gmail.com>
2024-02-20 10:31:00 +0330

    test publishing

--- tests/test_drafts.py
+++ tests/test_drafts.py
@@ -6,7 +6,7 @@ drafts_url = f"{settings.PREFIX}/{APIPrefixesEnum.DRAFTS.value}"
 
 def test_create_draft(client, admin_auth_headers):
     response = client.post(
-        f'{drafts_url}/create',
+        f"{drafts_url}/create",
         json={"title": "title", "body": "body"},
         headers=admin_auth_headers,
     )
@@ -127,3 +127,31 @@ def test_open_read(client, mahdi_auth_headers):
     assert data["title"] == "title"
     assert data["body"] == "body"
     assert data["username"] == "mahdi"
+
+
+def test_publish_post(client, mahdi_auth_headers):
+    draft_id = client.post(
+        f"{drafts_url}/create",
+        json={"title": "title", "body": "body"},
+        headers=mahdi_auth_headers,
+    ).json()["id"]
+    response = client.post(
+        f"{drafts_url}/publish/{draft_id}",
+        json={"tags": ["tag1", "tag2"], "slug": "my slug"},
+        headers=mahdi_auth_headers,
+    )
+    assert response.status_code == 200, response.text
+
+    data = response.json()
+    title, body, tags, published, updated = (
+        data["title"],
+        data["body"],
+        data["tags"],
+        data["published"],
+        data["updated"],
+    )
+    assert title == "title"
+    assert body == "body"
+    assert tags == ["tag1", "tag2"]
+    assert published
+    assert not updated
