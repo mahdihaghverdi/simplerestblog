b611ca720f3c5a708c20018d18fc456fe312cab4
mahdihaghverdi <mahdihaghverdiliewpl@gmail.com>
2024-03-03 10:25:57 +0330

    test `get_comments` route

--- tests/test_comments.py
+++ tests/test_comments.py
@@ -44,3 +44,78 @@ def test_add_reply(client, mahdi_auth_headers, post_id):
     assert data["parent_id"] == comment_id
     assert data["username"] == "mahdi"
     assert data["reply_count"] == 0
+
+
+def test_get_comments(client, mahdi_auth_headers, post_id):
+    c1_id = client.post(
+        f"{settings.PREFIX}/{APIPrefixesEnum.COMMENTS.value}/{post_id}",
+        json={"comment": "comment1"},
+        headers=mahdi_auth_headers,
+    ).json()["id"]
+
+    c2_id = client.post(
+        f"{settings.PREFIX}/{APIPrefixesEnum.COMMENTS.value}/{post_id}",
+        json={"comment": "comment2"},
+        headers=mahdi_auth_headers,
+    ).json()["id"]
+
+    _ = client.post(
+        f"{settings.PREFIX}/{APIPrefixesEnum.COMMENTS.value}/{post_id}",
+        json={"comment": "comment3"},
+        headers=mahdi_auth_headers,
+    ).json()["id"]
+
+    response = client.get(
+        f"{settings.PREFIX}/{APIPrefixesEnum.COMMENTS.value}/{post_id}",
+        params={"order": "first"},
+    )
+    assert response.status_code == 200, response.text
+
+    data = response.json()
+    assert len(data) == 3
+
+    for idx, cmt in enumerate(data, 1):
+        assert cmt["comment"] == f"comment{idx}"
+
+    response = client.get(
+        f"{settings.PREFIX}/{APIPrefixesEnum.COMMENTS.value}/{post_id}",
+        params={"order": "last"},
+    )
+    assert response.status_code == 200, response.text
+
+    data = response.json()
+    assert len(data) == 3
+
+    for idx, cmt in enumerate(reversed(data), 1):
+        assert cmt["comment"] == f"comment{idx}"
+
+    # add 2 rep to 1st
+    client.post(
+        f"{settings.PREFIX}/{APIPrefixesEnum.COMMENTS.value}/{post_id}/{c1_id}",
+        json={"comment": "rep1"},
+        headers=mahdi_auth_headers,
+    )
+    client.post(
+        f"{settings.PREFIX}/{APIPrefixesEnum.COMMENTS.value}/{post_id}/{c1_id}",
+        json={"comment": "rep2"},
+        headers=mahdi_auth_headers,
+    )
+
+    # add 1 rep to 2nd
+    client.post(
+        f"{settings.PREFIX}/{APIPrefixesEnum.COMMENTS.value}/{post_id}/{c2_id}",
+        json={"comment": "rep1"},
+        headers=mahdi_auth_headers,
+    )
+
+    response = client.get(
+        f"{settings.PREFIX}/{APIPrefixesEnum.COMMENTS.value}/{post_id}",
+        params={"order": "most_replied"},
+    )
+    assert response.status_code == 200, response.text
+
+    data = response.json()
+    assert len(data) == 3
+
+    for idx, cmt in enumerate(data, 1):
+        assert cmt["comment"] == f"comment{idx}"
