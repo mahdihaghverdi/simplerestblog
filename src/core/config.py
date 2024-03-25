79c91165e7f8208fc9b1ec270967b604da3932cb
mahdihaghverdi <mahdihaghverdiliewpl@gmail.com>
2024-03-25 21:17:26 +0330

    add REDIS_CACHE_URL to settings

--- src/core/config.py
+++ src/core/config.py
@@ -4,10 +4,11 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     DATABASE_URL: str
     TEST_DATABASE_URL: str
+    REDIS_CACHE_URL: str
     API_VERSION: str
     SECRET_KEY: str
     ALGORITHM: str
-    ACCESS_TOKEN_EXPIRE_MINUTES: int = 2 * 24 * 60  # two days
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 15
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
 
     @property
