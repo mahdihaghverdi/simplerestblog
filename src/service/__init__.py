730fb24af14e07d4fff42a4e73111748613f9df8
mahdihaghverdi <mahdihaghverdiliewpl@gmail.com>
2024-03-26 17:54:08 +0330

    add `redis_client` to `Service` class

--- src/service/__init__.py
+++ src/service/__init__.py
@@ -1,8 +1,11 @@
 from typing import TypeVar, Generic
 
+from src.core.redis_db import RedisClient
+
 R = TypeVar("R")
 
 
 class Service(Generic[R]):
-    def __init__(self, repo: R):
+    def __init__(self, repo: R, redis_client: RedisClient | None = None):
         self.repo: R = repo
+        self.redis_client = redis_client
