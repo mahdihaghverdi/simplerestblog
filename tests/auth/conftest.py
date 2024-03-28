ddc3cea524a54b7d0cc0d2084d52f372a0ade4ca
mahdihaghverdi <mahdihaghverdiliewpl@gmail.com>
2024-03-28 17:03:36 +0330

    test '/2fa-img'

--- tests/auth/conftest.py
+++ tests/auth/conftest.py
@@ -1,3 +1,5 @@
+from collections import namedtuple
+
 import pytest
 
 from src.core.enums import APIPrefixesEnum
@@ -5,8 +7,21 @@ from tests.conftest import base_url
 
 base_url = base_url + f"{APIPrefixesEnum.AUTH.value}"
 
+data = {"username": "mahdi", "password": "12345678"}
+
 
 @pytest.fixture
 def signup_mahdi(client):
-    client.post(base_url + "/signup", json={"username": "mahdi", "password": "12345678"})
+    client.post(base_url + "/signup", json=data)
     yield
+
+
+LoginData = namedtuple("LoginData", "refresh_token, csrf_token")
+
+
+@pytest.fixture
+def login_mahdi(client, signup_mahdi):
+    response = client.post(base_url + "/login", json=data)
+    yield LoginData(
+        response.cookies.get("Refresh-Token"), response.headers.get("x-csrf-token")
+    )
--- tests/auth/test_auth.py
+++ tests/auth/test_auth.py
@@ -1,3 +1,4 @@
+import base64
 from http import HTTPStatus
 
 from src.core.exceptions import CredentialsError
@@ -5,7 +6,6 @@ from src.core.schemas import UserOutSchema
 from src.core.security import decode_refresh_token, decode_csrf_token
 from .conftest import base_url
 
-signup_url = base_url + "/signup"
 username = "mahdi"
 password = "12345678"
 simple_signup_data = {"username": username, "password": password}
@@ -22,14 +22,47 @@ signup_data = {
 
 
 class BaseTest:
+    url: str
+
     @staticmethod
     def extract_error_message(data):
         return data["error"], data.get("details")
 
+    @staticmethod
+    def make_auth_headers(csrf_token):
+        return {"Authorization": f"Bearer {csrf_token}"}
+
+
+class RefreshTokenMixin:
+    def test_not_pass_refresh_token(self, client, login_mahdi):
+        response = client.post(
+            self.url, headers=self.make_auth_headers(login_mahdi.csrf_token)
+        )
+        assert response.status_code == HTTPStatus.FORBIDDEN.value, response.text
+
+        code_message, message = self.extract_error_message(response.json())
+        assert code_message == HTTPStatus.FORBIDDEN.description
+        assert message == "Refresh-Token is not provided"
+
+    def test_invalid_refresh_token(self, client, login_mahdi):
+        invalid_ref_token = login_mahdi.refresh_token.translate(str.maketrans({"a": "b"}))
+        response = client.post(
+            self.url,
+            headers=self.make_auth_headers(login_mahdi.csrf_token),
+            cookies={"Refresh-Token": invalid_ref_token},
+        )
+        assert response.status_code == HTTPStatus.UNAUTHORIZED.value, response.text
+
+        code_message, message = self.extract_error_message(response.json())
+        assert code_message == HTTPStatus.UNAUTHORIZED.description
+        assert message == "Invalid Refresh-Token"
+
 
 class TestSignup(BaseTest):
+    url = base_url + "/signup"
+
     def test_signup_simple(self, client):
-        response = client.post(signup_url, json=simple_signup_data)
+        response = client.post(self.url, json=simple_signup_data)
         assert response.status_code == 201, response.text
 
         data = response.json()
@@ -40,7 +73,7 @@ class TestSignup(BaseTest):
             assert v is None
 
     def test_signup_complex(self, client):
-        response = client.post(signup_url, json=signup_data)
+        response = client.post(self.url, json=signup_data)
         assert response.status_code == 201, response.text
 
         got = UserOutSchema(**response.json())
@@ -53,23 +86,24 @@ class TestSignup(BaseTest):
         assert got.twitter == f'https://x.com/@{signup_data["twitter"][1:]}'
 
     def test_signup_duplicate(self, client):
-        client.post(signup_url, json=simple_signup_data)
-        response = client.post(signup_url, json=simple_signup_data)
-        assert response.status_code == 400, response.text
+        client.post(self.url, json=simple_signup_data)
+        response = client.post(self.url, json=simple_signup_data)
+        assert response.status_code == HTTPStatus.BAD_REQUEST.value, response.text
 
         code_message, message = self.extract_error_message(response.json())
         assert code_message == HTTPStatus.BAD_REQUEST.description
         assert message == f"username: {username!r} already exists!"
 
 
-login_url = base_url + "/login"
 login_data = {"username": username, "password": password}
 
 
 class TestLogin(BaseTest):
+    url = base_url + "/login"
+
     def test_user_not_found_error(self, client):
-        response = client.post(login_url, json=login_data)
-        assert response.status_code == 404, response.text
+        response = client.post(self.url, json=login_data)
+        assert response.status_code == HTTPStatus.NOT_FOUND.value, response.text
 
         code_message, message = self.extract_error_message(response.json())
         assert code_message == HTTPStatus.NOT_FOUND.description
@@ -77,16 +111,16 @@ class TestLogin(BaseTest):
 
     def test_wrong_credentials(self, client, signup_mahdi):
         response = client.post(
-            login_url, json={"username": "mahdi", "password": "12345677"}
+            self.url, json={"username": "mahdi", "password": "12345677"}
         )
-        assert response.status_code == 401, response.text
+        assert response.status_code == HTTPStatus.UNAUTHORIZED.value, response.text
 
         code_message, message = self.extract_error_message(response.json())
         assert code_message == HTTPStatus.UNAUTHORIZED.description
         assert message == CredentialsError().message
 
     def test_login(self, client, signup_mahdi):
-        response = client.post(login_url, json=login_data)
+        response = client.post(self.url, json=login_data)
         assert response.status_code == 200, response.text
 
         refresh_token = response.cookies.get("Refresh-Token")
@@ -101,3 +135,18 @@ class TestLogin(BaseTest):
         csrf_token = decode_csrf_token(x_csrf_token)
         assert csrf_token.refresh_token == refresh_token
         assert csrf_token.access_token is None
+
+
+class TestQrImg(BaseTest, RefreshTokenMixin):
+    url = base_url + "/2fa-img"
+
+    def test_gen(self, client, login_mahdi):
+        response = client.post(
+            self.url,
+            headers=self.make_auth_headers(login_mahdi.csrf_token),
+            cookies={"Refresh-Token": login_mahdi.refresh_token},
+        )
+        assert response.status_code == 200, response.text
+
+        img = response.text
+        assert f'"{base64.b64encode(base64.b64decode(img)).decode()}"' == img
