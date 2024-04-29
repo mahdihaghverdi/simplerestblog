67d615e2b261dcd61686ba9496b79ce2eb5d64c6
mahdihaghverdi <mahdihaghverdiliewpl@gmail.com>
2024-04-29 16:51:16 +0330

    refactor drafts' tests

--- tests/test_drafts.py
+++ tests/test_drafts.py
@@ -1,36 +1,33 @@
 from src.core.config import settings
 from src.core.enums import APIPrefixesEnum
+from tests.conftest import BaseTest, create_draft, drafts_basic_url
 
 drafts_url = f"{settings.PREFIX}/{APIPrefixesEnum.DRAFTS.value}"
 
+bt = BaseTest()
 
-def test_create_draft(client, admin_auth_headers):
+
+def test_create_draft(client, refreshed_mahdi):
+    h, c = bt.headers_cookies_tuple(refreshed_mahdi)
     response = client.post(
-        f"{drafts_url}",
+        f"{drafts_basic_url}/",
+        headers=h,
+        cookies=c,
         json={"title": "title", "body": "body"},
-        headers=admin_auth_headers,
     )
     assert response.status_code == 201, response.text
 
     data = response.json()
     assert data["title"] == "title"
     assert data["body"] == "body"
-    assert data["username"] == "admin"
+    assert data["username"] == "mahdi"
 
 
-def test_get_all_drafts(client, mahdi_auth_headers):
-    def _():
-        return client.post(
-            f"{drafts_url}",
-            json={"title": "title", "body": "body"},
-            headers=mahdi_auth_headers,
-        ).json()["id"]
+def test_get_all_drafts(client, refreshed_mahdi):
+    h, c = bt.headers_cookies_tuple(refreshed_mahdi)
+    ids = [create_draft(client, h, c), create_draft(client, h, c)]
 
-    ids = [_(), _()]
-    response = client.get(
-        f"{drafts_url}/all",
-        headers=mahdi_auth_headers,
-    )
+    response = client.get(f"{drafts_url}/all", headers=h, cookies=c)
     assert response.status_code == 200, response.text
 
     data = response.json()
@@ -43,17 +40,11 @@ def test_get_all_drafts(client, mahdi_auth_headers):
         ]
 
 
-def test_get_one_draft(client, mahdi_auth_headers):
-    draft_id = client.post(
-        f"{drafts_url}",
-        json={"title": "title", "body": "body"},
-        headers=mahdi_auth_headers,
-    ).json()["id"]
+def test_get_one_draft(client, refreshed_mahdi):
+    h, c = bt.headers_cookies_tuple(refreshed_mahdi)
+    draft_id = create_draft(client, h, c)
 
-    response = client.get(
-        f"{drafts_url}/{draft_id}",
-        headers=mahdi_auth_headers,
-    )
+    response = client.get(f"{drafts_url}/{draft_id}", headers=h, cookies=c)
     assert response.status_code == 200, response.text
 
     data = response.json()
@@ -62,17 +53,15 @@ def test_get_one_draft(client, mahdi_auth_headers):
     assert data["username"] == "mahdi"
 
 
-def test_update_draft(client, mahdi_auth_headers):
-    draft_id = client.post(
-        f"{drafts_url}",
-        json={"title": "title", "body": "body"},
-        headers=mahdi_auth_headers,
-    ).json()["id"]
+def test_update_draft(client, refreshed_mahdi):
+    h, c = bt.headers_cookies_tuple(refreshed_mahdi)
+    draft_id = create_draft(client, h, c)
 
     response = client.put(
         f"{drafts_url}/{draft_id}",
         json={"title": "updated title", "body": "updated body"},
-        headers=mahdi_auth_headers,
+        headers=h,
+        cookies=c,
     )
     assert response.status_code == 200, response.text
 
@@ -82,44 +71,39 @@ def test_update_draft(client, mahdi_auth_headers):
     assert data["updated"] is not None
 
 
-def test_update_draft_not_found(client, mahdi_auth_headers):
+def test_update_draft_not_found(client, refreshed_mahdi):
+    h, c = bt.headers_cookies_tuple(refreshed_mahdi)
     response = client.put(
         f"{drafts_url}/1",
         json={"title": "updated title", "body": "updated body"},
-        headers=mahdi_auth_headers,
+        headers=h,
+        cookies=c,
     )
     assert response.status_code == 404, response.text
 
 
-def test_delete_draft(client, mahdi_auth_headers):
-    draft_id = client.post(
-        f"{drafts_url}",
-        json={"title": "title", "body": "body"},
-        headers=mahdi_auth_headers,
-    ).json()["id"]
+def test_delete_draft(client, refreshed_mahdi):
+    h, c = bt.headers_cookies_tuple(refreshed_mahdi)
+    draft_id = create_draft(client, h, c)
 
-    response = client.delete(
-        f"{drafts_url}/{draft_id}",
-        headers=mahdi_auth_headers,
-    )
+    response = client.delete(f"{drafts_url}/{draft_id}", headers=h, cookies=c)
     assert response.status_code == 204, response.text
 
 
-def test_delete_draft_not_found(client, mahdi_auth_headers):
-    response = client.delete(
-        f"{drafts_url}/1",
-        headers=mahdi_auth_headers,
-    )
+def test_delete_draft_not_found(client, refreshed_mahdi):
+    h, c = bt.headers_cookies_tuple(refreshed_mahdi)
+    response = client.delete(f"{drafts_url}/1", headers=h, cookies=c)
     assert response.status_code == 404, response.text
 
 
-def test_open_read(client, mahdi_auth_headers):
+def test_open_read(client, refreshed_mahdi):
+    h, c = bt.headers_cookies_tuple(refreshed_mahdi)
     draft_hash = client.post(
-        f"{drafts_url}",
+        f"{drafts_basic_url}/",
+        headers=h,
+        cookies=c,
         json={"title": "title", "body": "body"},
-        headers=mahdi_auth_headers,
     ).json()["draft_hash"]
-
     response = client.get(draft_hash)
     assert response.status_code == 200, response.text
 
@@ -129,17 +113,15 @@ def test_open_read(client, mahdi_auth_headers):
     assert data["username"] == "mahdi"
 
 
-def test_publish_post(client, mahdi_auth_headers):
-    draft_id = client.post(
-        f"{drafts_url}",
-        json={"title": "title", "body": "body"},
-        headers=mahdi_auth_headers,
-    ).json()["id"]
+def test_publish_post(client, refreshed_mahdi):
+    h, c = bt.headers_cookies_tuple(refreshed_mahdi)
+    draft_id = create_draft(client, h, c)
 
     response = client.post(
         f"{drafts_url}/publish/{draft_id}",
         json={"tags": ["tag1", "tag2"], "slug": "my slug"},
-        headers=mahdi_auth_headers,
+        headers=h,
+        cookies=c,
     )
     assert response.status_code == 200, response.text
 
@@ -158,29 +140,31 @@ def test_publish_post(client, mahdi_auth_headers):
     assert not updated
 
 
-def test_draft_published_before(client, mahdi_auth_headers):
-    draft_id = client.post(
-        f"{drafts_url}",
-        json={"title": "title", "body": "body"},
-        headers=mahdi_auth_headers,
-    ).json()["id"]
+def test_draft_published_before(client, refreshed_mahdi):
+    h, c = bt.headers_cookies_tuple(refreshed_mahdi)
+    draft_id = create_draft(client, h, c)
+
     client.post(
         f"{drafts_url}/publish/{draft_id}",
         json={"tags": ["tag1", "tag2"], "slug": "my slug"},
-        headers=mahdi_auth_headers,
+        headers=h,
+        cookies=c,
     )
     response = client.post(
         f"{drafts_url}/publish/{draft_id}",
         json={"tags": ["tag1", "tag2"], "slug": "my slug"},
-        headers=mahdi_auth_headers,
+        headers=h,
+        cookies=c,
     )
     assert response.status_code == 400, response.text
 
 
-def test_publish_but_draft_not_found(client, mahdi_auth_headers):
+def test_publish_but_draft_not_found(client, refreshed_mahdi):
+    h, c = bt.headers_cookies_tuple(refreshed_mahdi)
     response = client.post(
         f"{drafts_url}/publish/1",
         json={"tags": ["tag1", "tag2"], "slug": "my slug"},
-        headers=mahdi_auth_headers,
+        headers=h,
+        cookies=c,
     )
     assert response.status_code == 404, response.text
